3e629ed7eaf5fcf744c4e749ae24a74b442b4989
mavoropaev <m.a.voropaev@gmail.com>
2024-04-06 20:01:19 +0300

    first commit

--- src/main/java/com/vma/CrptApi.java
+++ src/main/java/com/vma/CrptApi.java
@@ -1,14 +1,73 @@
 package com.vma;
+import org.apache.http.HttpEntity;
+import org.apache.http.client.methods.CloseableHttpResponse;
+import org.apache.http.client.methods.HttpPost;
+import org.apache.http.entity.StringEntity;
+import org.apache.http.impl.client.CloseableHttpClient;
+import org.apache.http.impl.client.HttpClients;
+import org.apache.http.util.EntityUtils;
+import org.json.JSONObject;
 
+import java.util.concurrent.*;
 
-/**
- * Hello world!
- *
- */
-public class App
+import java.io.IOException;
+
+public class CrptApi
 {
-    public static void main( String[] args )
-    {
-        System.out.println( "Hello World!" );
+    private static final String API_URL = "https://ismp.crpt.ru/api/v3/lk/documents/create";
+
+    private final ScheduledExecutorService scheduler = Executors.newSingleThreadScheduledExecutor();
+    private final Semaphore semaphore;
+    private int requestLimit;
+
+    public CrptApi(int requestLimit, int intervalSeconds) {
+        scheduler.scheduleAtFixedRate(this::resetRequestCount, intervalSeconds, intervalSeconds, TimeUnit.SECONDS);
+        this.semaphore = new Semaphore(requestLimit);
+        this.requestLimit = requestLimit;
+    }
+
+    private void acquire() throws InterruptedException {
+        semaphore.acquire();
+    }
+
+    public void createDocument(String documentJson, String signature) throws InterruptedException {
+        acquire();
+        try (CloseableHttpClient httpClient = HttpClients.createDefault()) {
+            HttpPost httpPost = new HttpPost(API_URL);
+            httpPost.setHeader("Content-Type", "application/json");
+
+            // Добавляем параметр signature в JSON тело запроса
+            JSONObject requestBody = new JSONObject(documentJson);
+            requestBody.put("signature", signature);
+
+            StringEntity requestEntity = new StringEntity(requestBody.toString());
+            httpPost.setEntity(requestEntity);
+
+            try (CloseableHttpResponse response = httpClient.execute(httpPost)) {
+                HttpEntity responseEntity = response.getEntity();
+                System.out.println("Response code: " + response.getStatusLine().getStatusCode());
+                System.out.println("Response body: " + EntityUtils.toString(responseEntity));
+            }
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
+    }
+
+    private void resetRequestCount() {
+        semaphore.release(requestLimit);
+    }
+
+    public static void main(String[] args) {
+        CrptApi api = new CrptApi(5, 1); // Example: 5 requests per second
+        String documentJson = "{\"description\": { \"participantInn\": \"string\" }, \"doc_id\": \"string\", \"doc_status\": \"string\", \"doc_type\": \"LP_INTRODUCE_GOODS\", \"importRequest\": true, \"owner_inn\": \"string\", \"participant_inn\": \"string\", \"producer_inn\": \"string\", \"production_date\": \"2020-01-23\", \"production_type\": \"string\", \"products\": [ { \"certificate_document\": \"string\", \"certificate_document_date\": \"2020-01-23\", \"certificate_document_number\": \"string\", \"owner_inn\": \"string\", \"producer_inn\": \"string\", \"production_date\": \"2020-01-23\", \"tnved_code\": \"string\", \"uit_code\": \"string\", \"uitu_code\": \"string\" } ], \"reg_date\": \"2020-01-23\", \"reg_number\": \"string\"}";
+        String signature = "signature_string";
+        try {
+            for (int i = 0; i < 10; i++) {
+                api.createDocument(documentJson, signature);
+                Thread.sleep(200); // Simulate some delay between API calls
+            }
+        } catch (InterruptedException e) {
+            e.printStackTrace();
+        }
     }
 }
